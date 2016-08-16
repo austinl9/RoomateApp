f5407fe26226d21f53fe30bef1e1db091db3513f
Austin <acm.liu11@gmail.com>
2016-08-15 23:16:36 -0700

    Google login works

--- www/app/Login/google-ctrl.js
+++ www/app/Login/google-ctrl.js
@@ -1,35 +1,21 @@
+(function () {
+    'use strict';
 
-// //client id
-// // 1051787815971-ohb5htfb3o4jdkuv447i7gmscsrgjr2h.apps.googleusercontent.com 
+    angular.module('RoomateApp').controller('googleCtrl', ['$scope', '$ionicPopover', '$ionicModal', '$window', 'UserInfo', googleCtrl]);
 
-// //client secret
-// // 4TKycReFpRAcxTZOfqgA5DpH 
+    function googleCtrl($scope, $ionicPopover, $ionicModal, $window, UserInfo) {
+    
+
+        $scope.testmethod = function (){
+            console.log("hi");
 
 
-// (function () {
-//     'use strict';
+        };
 
-//     angular.module('RoomateApp', ['angular-google-gapi']).controller('googleCtrl', ['$scope', '$ionicPopover', '$ionicModal', '$window', 'UserInfo', 'GooglePlus', googleCtrl]);
 
-//     // angular.module('RoomateApp').config(['GooglePlusProvider', function (GooglePlusProvider) {
-//     //     GooglePlusProvider.init({
-//     //         clientId: '1051787815971-ohb5htfb3o4jdkuv447i7gmscsrgjr2h.apps.googleusercontent.com ',
-//     //         apiKey: '4TKycReFpRAcxTZOfqgA5DpH '
-//     //     });
-//     // }]);
-//     function googleCtrl($scope, $ionicPopover, $ionicModal, $window, UserInfo) {
-//         $scope.login = function () {
-//             GooglePlus.login().then(function (authResult) {
-//                 console.log(authResult);
 
-//                 GooglePlus.getUser().then(function (user) {
-//                     console.log(user);
-//                 });
-//             }, function (err) {
-//                 console.log(err);
-//             });
-//         };
 
-//     }
-// })();
+    }
+})();
+
 
