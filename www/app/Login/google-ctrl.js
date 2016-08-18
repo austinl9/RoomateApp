e774cb394c90e929917ccc6cf2aa1e0cb5167aa2
Austin <acm.liu11@gmail.com>
2016-08-17 22:34:19 -0700

    Google gets user information and picture

--- www/app/Login/google-ctrl.js
+++ www/app/Login/google-ctrl.js
@@ -4,18 +4,37 @@
     angular.module('RoomateApp').controller('googleCtrl', ['$scope', '$ionicPopover', '$ionicModal', '$window', 'UserInfo', googleCtrl]);
 
     function googleCtrl($scope, $ionicPopover, $ionicModal, $window, UserInfo) {
-    
 
-        $scope.testmethod = function (){
-            console.log("hi");
+        $scope.getUserInfo = function () {
+            gapi.client.request(
+                {
+                    'path': '/plus/v1/people/me',
+                    'method': 'GET',
+                    'callback': $scope.userInfoCallback
+                }
+            )
+        }
 
-
+        // When callback is received, process user info.
+        $scope.userInfoCallback = function (userInfo) {
+            $scope.$apply(function () {
+                $scope.processUserInfo(userInfo);
+            });
         };
 
-         $scope.$on('event:google-plus-signin-success', function (event,authResult) {
-    	        console.log("success bruh");
+        $scope.processUserInfo = function (userInfo) {
+            UserInfo.setUserName(userInfo.displayName);
+            UserInfo.setLoginStatus(true);
+            UserInfo.setPicture(userInfo.image.url);
+
+        }
+
+        $scope.$on('event:google-plus-signin-success', function (event, authResult) {
+            UserInfo.setLoginStatus(true);
+            console.log("success bruh");
+            $scope.getUserInfo();
         });
-        $scope.$on('event:google-plus-signin-failure', function (event,authResult) {
+        $scope.$on('event:google-plus-signin-failure', function (event, authResult) {
             console.log("failure bruh");
         });
 
